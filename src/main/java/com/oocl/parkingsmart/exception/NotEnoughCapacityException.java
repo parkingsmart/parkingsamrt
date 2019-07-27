4516ec1f16a516fe3bf7732569424fa0961c0fc5
xuexi666666 <235400554@qq.com>
2019-07-27 11:53:20 +0800

    git commit -m"定义后台停车场容量满的异常处理类"

--- src/main/java/com/oocl/parkingsmart/exception/NotEnoughCapacityException.java
+++ src/main/java/com/oocl/parkingsmart/exception/NotEnoughCapacityException.java
@@ -1,4 +1,9 @@
 package com.oocl.parkingsmart.exception;
 
-public class NotEnoughCapacityException {
+import org.springframework.http.HttpStatus;
+
+public class NotEnoughCapacityException extends SystemException {
+    public NotEnoughCapacityException(String parkingLotName) {
+        super(HttpStatus.NOT_ACCEPTABLE, String.format("the %s parking lot is not enough capacity", parkingLotName));
+    }
 }
