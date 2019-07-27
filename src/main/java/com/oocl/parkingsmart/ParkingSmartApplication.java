07407ba49f4ff9e6acf02a758eb3ae74f80d6160
xuexi666666 <235400554@qq.com>
2019-07-27 15:10:01 +0800

    git commit -m"[init] Write production, local, test configuration files"

--- src/main/java/com/oocl/parkingsmart/ParkingSmartApplication.java
+++ src/main/java/com/oocl/parkingsmart/ParkingSmartApplication.java
@@ -1,5 +1,11 @@
 package com.oocl.parkingsmart;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ParkingSmartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingSmartApplication.class, args);
+    }
 }
