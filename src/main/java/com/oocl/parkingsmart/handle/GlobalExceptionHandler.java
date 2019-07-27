8b6799b690e9ce0b156a5283683efd5caedb1631
xuexi666666 <235400554@qq.com>
2019-07-27 12:06:35 +0800

    git commit -m"定义后台全局异常处理类"

---
+++ src/main/java/com/oocl/parkingsmart/handle/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.oocl.parkingsmart.handle;
+
+import com.oocl.parkingsmart.exception.SystemException;
+import org.hibernate.JDBCException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(SystemException.class)
+    @ResponseBody
+    public ResponseEntity systemExceptionHandler(SystemException err){
+        return ResponseEntity.status(err.getErrorCode()).build();
+    }
+
+    @ExceptionHandler(JDBCException.class)
+    @ResponseBody
+    public ResponseEntity jdbcExceptionHandler(JDBCException err){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(err.toString());
+    }
+}
