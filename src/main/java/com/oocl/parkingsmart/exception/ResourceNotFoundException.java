a2d679490005ed0a74cb8cfad5175ed88a958f9f
xuexi666666 <235400554@qq.com>
2019-07-27 12:03:52 +0800

    git commit -m"定义后台资源不存在异常处理类"

--- src/main/java/com/oocl/parkingsmart/exception/ResourceNotFoundException.java
+++ src/main/java/com/oocl/parkingsmart/exception/ResourceNotFoundException.java
@@ -1,4 +1,9 @@
 package com.oocl.parkingsmart.exception;
 
-public class ResourceNotFoundException {
+import org.springframework.http.HttpStatus;
+
+public class ResourceNotFoundException extends SystemException {
+    ResourceNotFoundException() {
+        super(HttpStatus.NOT_FOUND,"所需资源不存在");
+    }
 }
