448ae2b6104f70585557065bc46fdd8a490ead15
Tiger Zhu <tiger.zhu@oocl.com>
2019-07-28 18:28:20 +0800

    [ feat ] add a grab order function

--- src/main/java/com/oocl/parkingsmart/service/OrderService.java
+++ src/main/java/com/oocl/parkingsmart/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.oocl.parkingsmart.service;
 
+import com.oocl.parkingsmart.entity.Employee;
 import com.oocl.parkingsmart.entity.Order;
 import com.oocl.parkingsmart.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,21 +17,28 @@ public class OrderService {
 
     public static final int PAGE_SIZE = 10;
 
-    public int getAllOrdersNum(){
+    public int getAllOrdersNum() {
         return orderRepository.findAll().size();
     }
 
-    public List<Order> getPageOrders(int page){
-        PageRequest pageRequest = PageRequest.of(page-1,PAGE_SIZE);
+    public List<Order> getPageOrders(int page) {
+        PageRequest pageRequest = PageRequest.of(page - 1, PAGE_SIZE);
         return orderRepository.findAll(pageRequest).getContent();
     }
 
-    public Order addOrder(Order order){
+    public Order addOrder(Order order) {
         Order order1 = orderRepository.save(order);
         return order1;
     }
 
     public List<Order> getAllNewOrders() {
         return orderRepository.getNewOrders();
+    }
+
+    public void grabOrderById(Long id, Employee employee) {
+        Order order = orderRepository.findById(id).get();
+        order.setEmployeeId(employee.getId());
+        order.setStatus(1);
+        orderRepository.save(order);
     }
 }
