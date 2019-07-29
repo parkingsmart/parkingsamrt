3f414a487492749edd0c227101b1b72f97673132
xuexi666666 <235400554@qq.com>
2019-07-29 18:16:23 +0800

    feat add UserController

---
+++ src/main/java/com/oocl/parkingsmart/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.oocl.parkingsmart.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RequestMapping("/api/users")
+@RestController
+public class UserController {
+
+}
