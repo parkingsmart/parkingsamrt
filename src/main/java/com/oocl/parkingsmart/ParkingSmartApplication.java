a60ae8ca9be1e11788cbeca8139c602abcebb218
xuexi666666 <235400554@qq.com>
2019-07-30 14:23:13 +0800

    init

--- src/main/java/com/oocl/parkingsmart/ParkingSmartApplication.java
+++ src/main/java/com/oocl/parkingsmart/ParkingSmartApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ParkingSmartApplication {
     public static void main(String[] args) {
         SpringApplication.run(ParkingSmartApplication.class, args);
