9037d2ff2a693518fa3384efe1d857d1f12534c8
montatLeast <314099319@qq.com>
2019-08-01 16:39:54 +0800

    [style] modify exception message to Chinese

--- src/main/java/com/oocl/parkingsmart/exception/PasswordValidException.java
+++ src/main/java/com/oocl/parkingsmart/exception/PasswordValidException.java
@@ -4,6 +4,6 @@ import org.springframework.http.HttpStatus;
 
 public class PasswordValidException extends SystemException {
     public PasswordValidException() {
-        super(HttpStatus.BAD_REQUEST,"wrong user password");
+        super(HttpStatus.BAD_REQUEST,"用户密码错误");
     }
 }
